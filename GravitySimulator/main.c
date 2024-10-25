e44cf9df795be6fe8a0dfd4886e53b509b797fc6
ElArquitectorgo <mega2814s@gmail.com>
2024-10-25 20:10:31 +0200

    Added honorable mention

--- GravitySimulator/main.c
+++ GravitySimulator/main.c
@@ -12,6 +12,9 @@
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
+// Heavily inspired in William Y. Feng's implementation
+// See https://github.com/womogenes/GravitySim
+
 enum Shape {
     Spiral,
     Sphere,
